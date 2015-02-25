63be1749253446ced36e0026f3be70ce1a9f260c
cidpas31 <cidpas@gmail.com>
2015-02-25 16:32:54 +0100

    creation ma premiere page html. formulaire avec nom prenom. aussi un boite qui appell un java script

--- WebApplication_first_html5/JavaScript1.js
+++
@@ -1,5 +0,0 @@
-﻿
-function TestJs1() {
-    alert("rouge");
-    var res = confirm("voulez vous coucher avec moi ce soir");
-}
---
+++ WebApplication_first_html5/script/JavaScript1.js
@@ -0,0 +1,17 @@
+﻿
+function TestJs1() {
+    alert("rouge");
+    var res = confirm("voulez vous coucher avec moi ce soir");
+}
+
+function afficher_text()
+{
+    var text_box1 = document.getElementById("first-name");
+    if (text_box1.value != "") {
+        confirm("yeah voila le nom de la textbox id first-name :" + text_box1.value);
+    }
+    else {
+        alert("rien dans la case first name");
+    } 
+
+}
